952c80cfb67fb3a04f41ca4dfc6b1ab42d619a6e
Al-kodach <alkodach351@gmail.com>
2023-03-31 18:12:59 -0400

    add-getPrice-func

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def enterProducts(name, quantity):
+#!/usr/bin/env python3
+
+def enterProducts():
     buyingData = {}
     enterDetails = True
 
@@ -13,4 +15,24 @@ def enterProducts(name, quantity):
         elif details == 'Q':
             enterDetails = False
 
+        else:
+            print('Please select a correct option')
+
     return buyingData
+
+def getPrice(product, quantity):
+    priceData = {
+        'Biscuit': 2,
+        'Chicken': 6,
+        'Egg': 1,
+        'Fist': 4,
+        'Coke': 2,
+        'Bread': 2,
+        'Apple': 3,
+        'Onion': 3
+    }
+
+    subtotal = priceData[product] * quantity
+    print(product + ':$' + str(priceData[product]) + 'x' + str(quantity) + '=' + str(subtotal))
+
+    return subtotal
