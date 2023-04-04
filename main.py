a8ba17e627a696d8592e6243b2c3390f6b984676
Al-kodach <alkodach351@gmail.com>
2023-04-04 07:54:35 -0400

    add-getDiscount-makeBill-fn-final

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-
+# we got cashier to input all products and store it in a dictionary
+# each products value is the quantity
 def enterProducts():
     buyingData = {}
     enterDetails = True
 
+    # this loop will help cashier to continue entering product or disconnect from entering product
     while enterDetails:
         details = input(('Press A to add product or Q to quite:'))
 
         if details == 'A':
             product = input('Enter product:')
             quantity = int(input('Enter quantity:'))
+
+            # we update the dictionary with the product name and quantity bought
             buyingData.update({product: quantity})
 
         elif details == 'Q':
@@ -20,6 +24,8 @@ def enterProducts():
 
     return buyingData
 
+
+# This function returns the subtotal of a single product as per it's price quantity.
 def getPrice(product, quantity):
     priceData = {
         'Biscuit': 2,
@@ -33,6 +39,58 @@ def getPrice(product, quantity):
     }
 
     subtotal = priceData[product] * quantity
-    print(product + ':$' + str(priceData[product]) + 'x' + str(quantity) + '=' + str(subtotal))
+    print(product + ':$' +
+          str(priceData[product]) + 'x' + str(quantity) + '=' + str(subtotal))
 
     return subtotal
+
+
+# This function decide if customer is qualified for a discount or not.
+def getDiscount(billAmount, membership):
+    discount = 0
+
+    if billAmount >= 25:
+        if membership == 'Gold':
+            billAmount *= 0.80
+            discount = 20
+
+        elif membership == 'Silver':
+            billAmount *= 0.90
+            discount = 10
+
+        elif membership == 'Bronze':
+            billAmount *= 0.95
+            discount = 5
+        print(str(discount) + ' % off for ' + membership + ' ' +
+              'membership on total amount: $' + str(billAmount))
+
+    else:
+        print('No discount on amount less than $25')
+    return billAmount
+
+
+
+def makeBill(buyingData, membership):
+    billAmount = 0
+
+    # we extract products name and quantity
+    for key, value in buyingData.items():
+        # we pass product name and quantity to getPrice() with returns a subtotal for each product
+				# we add to billAmout for each subtotal
+        billAmount += getPrice(key, value)
+
+				# Finally we call getDiscount() and pass the total amount of products bought, this will return the discounted bill
+        billAmount = getDiscount(billAmount, membership)
+
+        print('The discounted amount is $:' + str(billAmount))
+
+
+# The first line of excution will start from here
+# Makes call to enterProduct() and store all product in buyingData varieble:
+buyingData = enterProducts()
+
+# This let cashier enters customers membership
+membership = input('Enter customers membership:')
+
+# we make a call to makeBill() passing products and membership as arguments
+makeBill(buyingData, membership)
